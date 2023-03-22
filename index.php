4c001fb770fbf1c02f0318698aee754cd210c0eb
HenryKim2022 <autromatron@gmail.com>
2023-03-22 14:09:14 +0700

    1

--- index.php
+++ index.php
@@ -332,8 +332,6 @@ $Age = (date("md", date("U", mktime(0, 0, 0, $BirthD_alias[0], $BirthD_alias[1],
     <a id="src-downloader" class="src-downloader d-flex align-items-center justify-content-center active">
         <i class="bx bx-cloud-download"></i>
     </a>
-
-
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center active">
         <i class="bi bi-arrow-up-short"></i>
     </a>
